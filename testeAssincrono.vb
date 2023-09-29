cb1d2d0fc7596de90dbbc8c7536e7f208a733e23
Tiago 3 <tiago-tmt@hotmail.com>
2023-09-29 17:16:24 -0300

    +testeAssincrono.vb

---
+++ testeAssincrono.vb
@@ -0,0 +1,32 @@
+﻿Public Class testeAssincrono
+
+    Friend Sub chamadaAssincrona()
+
+        Console.WriteLine("Início do programa")
+
+        ' Iniciar uma Task assíncrona
+        Dim resultado As Task = MinhaTarefaAsync()
+
+        ' Continuar a execução de outras tarefas na thread principal
+        Console.WriteLine("Outras tarefas na thread principal")
+
+        Console.WriteLine("Outras tarefas na thread principal 2")
+
+        ' Aguardar a conclusão da Task antes de encerrar o programa
+        resultado.Wait()
+
+        Console.WriteLine("Fim do programa")
+
+    End Sub
+
+    Private Async Function MinhaTarefaAsync() As Task
+
+        Console.WriteLine("Início da MinhaTarefaAsync")
+
+        ' Simular uma operação assíncrona com Await Task.Delay
+        Await Task.Delay(2000) ' Aguarda por 2 segundos de forma assíncrona
+
+        Console.WriteLine("Fim da MinhaTarefaAsync")
+
+    End Function
+End Class
